f4924623cb764dc2174e09d314eb0117d193d201
DonluizKv <luizkv4@outlook.comm>
2024-04-12 19:00:49 -0500

    fixed new error in another push

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			width: {
+				"30":"30%",
+				"70":"70%"
+			},
+		},
 	},
 	plugins: [],
 }
